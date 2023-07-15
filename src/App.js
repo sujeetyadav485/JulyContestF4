34cbde884080dc7758e324c77d58c768dcddbeb3
Sujeet Yadav <99132296+sujeetyadav485@users.noreply.github.com>
2023-07-15 22:29:01 +0530

    Contest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Sign.css'
 
-function App() {
+function Sign() {
+  const [formData, setFormData] = useState({
+    email: '',
+    name: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newErrors = {};
+
+    // Basic form validation
+    if (formData.email.trim() === '') {
+      newErrors.email = 'Email is required.';
+    }
+
+    if (formData.name.trim() === '') {
+      newErrors.name = 'Name is required.';
+    }
+
+    if (formData.password.trim() === '') {
+      newErrors.password = 'Password is required.';
+    }
+
+    if (formData.confirmPassword.trim() === '') {
+      newErrors.confirmPassword = 'Confirm Password is required.';
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      newErrors.confirmPassword = 'Passwords do not match.';
+    }
+
+    setErrors(newErrors);
+
+    // If no errors, proceed with form submission
+    if (Object.keys(newErrors).length === 0) {
+      // TODO: Handle form submission logic here
+      console.log('Form submitted:', formData);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {errors.email && <div className="error">{errors.email}</div>}
+        </div>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          {errors.name && <div className="error">{errors.name}</div>}
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          {errors.password && <div className="error">{errors.password}</div>}
+        </div>
+        <div>
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+          {errors.confirmPassword && (
+            <div className="error">{errors.confirmPassword}</div>
+          )}
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default Sign;
